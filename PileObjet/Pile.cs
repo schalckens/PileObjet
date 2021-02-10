76b2b7acf57793c53247f1a35b15213a62fa43a5
Valentine SCHALCKENS <v.schalckens@gmail.com>
2021-02-10 20:48:12 +0100

    3

--- PileObjet/Pile.cs
+++ PileObjet/Pile.cs
@@ -1,6 +1,12 @@
 ﻿namespace MesOutils
 {
+    /// <summary>
+    /// Pile pouvant contenir des entiers.
+    /// On ajoute après le dernier éléments ajouté.
+    /// On retire le dernier élément ajouté
+    /// </summary>
     class Pile
     {
+
     }
 }
