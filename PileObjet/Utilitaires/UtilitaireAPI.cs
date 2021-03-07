64462a0993596d230a20f55673efab038d0f367f
Valentine SCHALCKENS <v.schalckens@gmail.com>
2021-03-07 19:42:48 +0100

    Ajout de la classe UtilitaireAPI, ajout de la méthode InversePhrase dans la classe UtilitaireConsole et ajout de la méthode TestInversePhrase dans la classe TestPile

---
+++ PileObjet/Utilitaires/UtilitaireAPI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilitaires
+{
+    public static class UtilitaireAPI
+    {
+        /// <summary>
+        /// Génère un paragraphe lorem ipsum à partir de l'API REST
+        /// </summary>
+        /// <param name="nbParagraphes"></param>
+        /// <returns></returns>
+        public static String RecupereLoremIpsum(int nbParagraphes)
+        {
+            HttpClient client = new HttpClient(); // Classe qui envoie des requêtes HTTP et reçoit les réponses
+
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/text"));
+            String url = $"https://loripsum.net/api/%7BnbParagraphes%7D/short/plaintext";
+            var response = client.GetAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = response.Content.ReadAsStringAsync().Result;
+
+                return responseBody;
+            }
+            else
+            {
+                throw new Exception("erreur API : " + response.StatusCode + " " + response.ReasonPhrase);
+            }
+        }
+    }
+}
